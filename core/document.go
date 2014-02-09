00182c71175a7f94b11b0f6b30fb4175629916f9
Shinichi Katayama <shin1.katayama@gmail.com>
2014-02-08 18:49:49 -0800

    Implement Insert and Delete

--- core/document.go
+++ core/document.go
@@ -15,3 +15,51 @@ func NewGapBuffer() *GapBuffer {
 		gapSize:   INITIAL_GAP_SIZE,
 	}
 }
+
+func (b *GapBuffer) confirmGap(newGapOffset uint) {
+	if b.gapSize == 0 {
+		b.gapOffset = uint(len(b.buffer))
+		b.buffer = make([]byte, len(b.buffer)+INITIAL_GAP_SIZE)
+		b.gapSize = INITIAL_GAP_SIZE
+	}
+
+	if newGapOffset < b.gapOffset {
+		len := b.gapOffset - newGapOffset
+		copy(b.buffer[newGapOffset+b.gapSize:], b.buffer[newGapOffset:newGapOffset+len])
+	} else {
+		len := newGapOffset - b.gapOffset
+		copy(b.buffer[b.gapOffset:], b.buffer[b.gapOffset+b.gapSize:b.gapOffset+b.gapSize+len])
+	}
+	b.gapOffset = newGapOffset
+}
+
+func (gb *GapBuffer) Insert(offset uint, by byte) {
+	gb.confirmGap(offset)
+	gb.buffer[offset] = by
+	gb.gapOffset++
+	gb.gapSize--
+}
+
+func (gb *GapBuffer) Delete(offset uint) {
+	if gb.Len() == 0 {
+		return
+	}
+
+	gb.confirmGap(offset + 1)
+	gb.gapOffset--
+	gb.gapSize++
+}
+
+func (gb *GapBuffer) Len() uint {
+	return uint(len(gb.buffer)) - gb.gapSize
+}
+
+func (gb *GapBuffer) Get(index uint) byte {
+	var i uint = 0
+	if index < gb.gapOffset {
+		i = index
+	} else {
+		i = gb.gapSize + index
+	}
+	return gb.buffer[i]
+}
--- core/document_test.go
+++ core/document_test.go
@@ -5,8 +5,32 @@ import (
 )
 
 func TestNewGapBuffer(t *testing.T) {
-	buf := NewGapBuffer()
-	if buf == nil {
+	// when
+	gb := NewGapBuffer()
+
+	// then
+	if gb == nil {
+		t.Error("buf should not be nil")
+	}
+	if gb.Len() != 0 {
+		t.Error("Len should be 0")
+	}
+}
+
+func TestInsert(t *testing.T) {
+	gb := NewGapBuffer()
+	if gb.Len() != 0 {
+		t.Error("Len should be 0")
+	}
+
+	// when
+	gb.Insert(0, 'a')
+
+	// then
+	if gb.Len() != 1 {
+		t.Error("buf should not be nil")
+	}
+	if gb.Get(0) != 'a' {
 		t.Error("buf should not be nil")
 	}
 }
