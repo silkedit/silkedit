a6e5c736abf529b7c4ef2ad0ca696987b4fd0842
Shinichi Katayama <shin1.katayama@gmail.com>
2014-02-19 00:13:48 -0800

    Separate DocumentView

---
+++ cui/document_view.go
@@ -0,0 +1,42 @@
+package cui
+
+import (
+	"../core"
+	"github.com/golang/glog"
+	"github.com/nsf/termbox-go"
+	"github.com/shinichy/go-wcwidth"
+)
+
+type DocumentView struct {
+	doc    core.Document
+	line   int
+	column int
+}
+
+func NewDocumentView() *DocumentView {
+	view := DocumentView{
+		doc:    core.NewDocument(),
+		line:   0,
+		column: 0,
+	}
+	view.doc.Subscribe(view.draw)
+	return &view
+}
+
+func (v *DocumentView) Insert(r rune) {
+	glog.Infof("Insert: %v", r)
+	n := v.doc.Insert(uint(v.column), r)
+	v.column += n
+}
+
+func (v *DocumentView) draw() {
+	const coldef = termbox.ColorDefault
+	termbox.Clear(coldef, coldef)
+	i := 0
+	v.doc.ForEach(func(r rune) {
+		termbox.SetCell(i, v.line, r, coldef, coldef)
+		i += wcwidth.Wcwidth(r)
+	})
+
+	termbox.Flush()
+}
--- main.go
+++ main.go
@@ -1,46 +1,11 @@
 package main
 
 import (
-	"./core"
+	"./cui"
 	"flag"
 	"github.com/golang/glog"
 	"github.com/nsf/termbox-go"
-	"github.com/shinichy/go-wcwidth"
 )
-
-type DocumentView struct {
-	doc    core.Document
-	line   int
-	column int
-}
-
-func NewDocumentView() *DocumentView {
-	view := DocumentView{
-		doc:    core.NewDocument(),
-		line:   0,
-		column: 0,
-	}
-	view.doc.Subscribe(view.draw)
-	return &view
-}
-
-func (v *DocumentView) Insert(r rune) {
-	glog.Infof("Insert: %v", r)
-	n := v.doc.Insert(uint(v.column), r)
-	v.column += n
-}
-
-func (v *DocumentView) draw() {
-	const coldef = termbox.ColorDefault
-	termbox.Clear(coldef, coldef)
-	i := 0
-	v.doc.ForEach(func(r rune) {
-		termbox.SetCell(i, v.line, r, coldef, coldef)
-		i += wcwidth.Wcwidth(r)
-	})
-
-	termbox.Flush()
-}
 
 func main() {
 	flag.Parse()
@@ -52,7 +17,7 @@ func main() {
 	defer termbox.Close()
 	termbox.SetInputMode(termbox.InputEsc | termbox.InputMouse)
 
-	view := NewDocumentView()
+	view := cui.NewDocumentView()
 mainloop:
 	for {
 		switch ev := termbox.PollEvent(); ev.Type {
