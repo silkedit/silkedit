fd32e0ff6978aa78d930f2c5a1676dbeba696580
Shinichi Katayama <shin1.katayama@gmail.com>
2015-01-26 20:03:53 +0900

    Add WA_TransparentForMouseEvents in FakeWindow

--- src/FakeWindow.cpp
+++ src/FakeWindow.cpp
@@ -13,6 +13,9 @@ FakeWindow::FakeWindow(TabBar* tabbar, const QPoint& dragStartPos) {
   setWindowFlags(Qt::Widget | Qt::FramelessWindowHint | Qt::X11BypassWindowManagerHint);
   setAttribute(Qt::WA_DeleteOnClose);
   setAttribute(Qt::WA_ShowWithoutActivating);
+  // WA_TransparentForMouseEvents should pass through mouse event but it doens't work as expected on Mac
+  // https://bugreports.qt.io/browse/QTBUG-41696
+  setAttribute(Qt::WA_TransparentForMouseEvents);
 
   QRect rect = tabbar->tabRect(tabbar->currentIndex());
   QPixmap pixmap = tabbar->grab(rect);
