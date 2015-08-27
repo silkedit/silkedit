01cbae1de88b14ff10ebe15ab675f2abccb96bd5
Shinichi Katayama <shin1.katayama@gmail.com>
2015-08-27 18:11:00 +0900

    Sort encodings list

--- src/EncodingComboBox.cpp
+++ src/EncodingComboBox.cpp
@@ -5,6 +5,8 @@ EncodingComboBox::EncodingComboBox(QWidget* parent) : QComboBox(parent) {
   for (const Encoding& encoding : Encoding::availableEncodings()) {
     addItem(encoding.displayName(), encoding.name());
   }
+  // Separator between default encoding + 3 Japanese encodings and others
+  insertSeparator(4);
 }
 
 void EncodingComboBox::setCurrentEncoding(const Encoding& encoding) {
