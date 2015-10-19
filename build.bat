50f469da958f34c1250e0891b7974938d8e596ce
Shinichi Katayama <shin1.katayama@gmail.com>
2015-10-19 18:48:51 +0900

    Add u8 prefix to Japanese strings to workaround MSVC build issue.

--- build.bat
+++ build.bat
@@ -1,13 +1,13 @@
 if "%PROCESSOR_ARCHITECTURE%" == "x86" (
-  if "%2"=="edge" (
-    "C:\Program Files\Microsoft Visual Studio 14.0\VC\vcvarsall.bat" %1 & cmake -DCMAKE_EXPORT_COMPILE_COMMANDS=ON -DCMAKE_BUILD_TYPE=Release -DBUILD_EDGE=ON -G "NMake Makefiles" .. & C:\jom\jom"
+  if "%3"=="edge" (
+    "C:\Program Files\Microsoft Visual Studio 14.0\VC\vcvarsall.bat" %1 & cmake -DCMAKE_EXPORT_COMPILE_COMMANDS=ON -DCMAKE_BUILD_TYPE=Release -DBUILD_EDGE=ON -G "NMake Makefiles" .. & C:\jom\jom %2
   ) else (
-    "C:\Program Files\Microsoft Visual Studio 14.0\VC\vcvarsall.bat" %1 & cmake -DCMAKE_EXPORT_COMPILE_COMMANDS=ON -DCMAKE_BUILD_TYPE=Release -G "NMake Makefiles" .. & C:\jom\jom"
+    "C:\Program Files\Microsoft Visual Studio 14.0\VC\vcvarsall.bat" %1 & cmake -DCMAKE_EXPORT_COMPILE_COMMANDS=ON -DCMAKE_BUILD_TYPE=Release -G "NMake Makefiles" .. & C:\jom\jom %2
   )
 ) else (
-  if "%2"=="edge" (
-    "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\vcvarsall.bat" %1 & cmake -DCMAKE_EXPORT_COMPILE_COMMANDS=ON -DCMAKE_BUILD_TYPE=Release -DBUILD_EDGE=ON -G "NMake Makefiles" .. & C:\jom\jom"
+  if "%3"=="edge" (
+    "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\vcvarsall.bat" %1 & cmake -DCMAKE_EXPORT_COMPILE_COMMANDS=ON -DCMAKE_BUILD_TYPE=Release -DBUILD_EDGE=ON -G "NMake Makefiles" .. & C:\jom\jom %2
   ) else (
-    "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\vcvarsall.bat" %1 & cmake -DCMAKE_EXPORT_COMPILE_COMMANDS=ON -DCMAKE_BUILD_TYPE=Release -G "NMake Makefiles" .. & C:\jom\jom"
+    "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\vcvarsall.bat" %1 & cmake -DCMAKE_EXPORT_COMPILE_COMMANDS=ON -DCMAKE_BUILD_TYPE=Release -G "NMake Makefiles" .. & C:\jom\jom %2
   )
 )
