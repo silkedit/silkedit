a5134397bf864ad635858f2091e4a4dac7f5993a
mizuki-y <mizuki-y@syati.info>
2015-11-21 14:23:14 +0900

    Refactor func name

--- src/TabView.cpp
+++ src/TabView.cpp
@@ -42,7 +42,7 @@ TabView::TabView(QWidget* parent)
   setMovable(true);
   setDocumentMode(true);
   setTabsClosable(true);
-  changeTabStyle(Config::singleton().theme());
+  setTheme(Config::singleton().theme());
   // Note: setDocumentMode also calls setDrawBase
   tabBar()->setDrawBase(false);
 
@@ -52,7 +52,7 @@ TabView::TabView(QWidget* parent)
   connect(this, &QTabWidget::tabBarClicked, this, &TabView::focusTabContent);
   connect(this, &QTabWidget::currentChanged, this, &TabView::changeActiveEditView);
   connect(this, &QTabWidget::tabCloseRequested, this, &TabView::removeTabAndWidget);
-  connect(&Config::singleton(), &Config::themeChanged, this, &TabView::changeTabStyle);
+  connect(&Config::singleton(), &Config::themeChanged, this, &TabView::setTheme);
 }
 
 TabView::~TabView() {
@@ -252,7 +252,7 @@ void TabView::changeTabText(const QString& path) {
   }
 }
 
-void TabView::changeTabStyle(Theme* theme) {
+void TabView::setTheme(Theme* theme) {
   if (theme) {
     ColorSettings* settings = theme->scopeSettings.first()->colorSettings.get();
     if (settings->contains("background")) {
