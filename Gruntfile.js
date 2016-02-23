af6474e47fc903c10265e292568fc5f687d91529
Shinichi Katayama <shin1.katayama@gmail.com>
2016-02-23 12:28:51 +0900

    Run 'npm run minify' in build task. Move default package to jslib/node_modules/silkedit/node_modules.

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,20 +3,13 @@ module.exports = function (grunt) {
 
     // define source files and their destinations
     uglify: {
-			jslib: {
-            src: 'minified/jslib/**/*.js',  // source files mask
+        jslib: {
+            src: ['minified/jslib/**/*.js', '!minified/jslib/node_modules/silkedit/node_modules/default/resources/hello/**/*.js'],  // source files mask
             dest: '.',    // destination folder
             expand: true,    // allow dynamic building
             flatten: false,   // remove all unnecessary nesting
             ext: '.js'   // replace .js to .min.js
-			},
-			defualt: {
-            src: ['minified/default/index.js', 'minified/default/lib/**/*.js'],  // source files mask
-            dest: '.',    // destination folder
-            expand: true,    // allow dynamic building
-            flatten: false,   // remove all unnecessary nesting
-            ext: '.js'   // replace .js to .min.js
-			}
+        }
     }
 });
 
