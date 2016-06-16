3d29002af4c517219059741c562b717789610232
Shinichi Katayama <shin1.katayama@gmail.com>
2016-06-16 17:08:13 +0900

    Validate a package name when entering a new package name. Add Validator class

--- widgets/src/LineEdit.cpp
+++ widgets/src/LineEdit.cpp
@@ -10,6 +10,18 @@ LineEdit::LineEdit(const QString& contents, QWidget* parent) : QLineEdit(content
   setClearButtonEnabled(true);
 }
 
+bool LineEdit::hasAcceptableInput() const {
+  return QLineEdit::hasAcceptableInput();
+}
+
+void LineEdit::setValidator(const QValidator* v) {
+  QLineEdit::setValidator(v);
+}
+
+const QValidator* LineEdit::validator() const {
+  return QLineEdit::validator();
+}
+
 void LineEdit::keyPressEvent(QKeyEvent* event) {
   switch (event->key()) {
     case Qt::Key_Return:
