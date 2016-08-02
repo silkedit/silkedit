606221d0155f98f64f39e881666323179571bfeb
Shinichi Katayama <shin1.katayama@gmail.com>
2016-08-02 14:34:01 +0900

    Add a link of the license url instead of showing it with QPlainTextEdit

--- widgets/src/AboutDialog.cpp
+++ widgets/src/AboutDialog.cpp
@@ -9,15 +9,8 @@
 AboutDialog::AboutDialog(QWidget* parent) : QDialog(parent), ui(new Ui::AboutDialog) {
   ui->setupUi(this);
 
-  QFile license(":/LICENSE.md");
-  if (!license.open(QIODevice::ReadOnly)) {
-    qCritical() << "failed to open LICENSE in resource";
-    return;
-  }
-
-  ui->licenseTextEdit->setPlainText(license.readAll());
   ui->buildLabel->setText(tr("version") + " " + App::applicationVersion() + " (" + tr("build") +
-                         ": " + BUILD + ")");
+                          ": " + BUILD + ")");
 }
 
 AboutDialog::~AboutDialog() {
