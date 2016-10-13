dc5c55bdb367cd4d312d93af0e7ad7af440af637
Shinichi Katayama <shin1.katayama@gmail.com>
2016-10-13 22:05:01 +0900

    Use msbuild on Windows because jom is unstable in appveyor.

--- build.bat
+++ build.bat
@@ -1,5 +1,5 @@
-if "%3"=="edge" (
-  "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\vcvarsall.bat" %1 & cmake -DCMAKE_EXPORT_COMPILE_COMMANDS=ON -DCMAKE_BUILD_TYPE=Release -DBUILD_EDGE=ON -G "NMake Makefiles" .. & jom %2
+if "%4"=="edge" (
+  "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\vcvarsall.bat" %1 & cmake -DCMAKE_EXPORT_COMPILE_COMMANDS=ON -DCMAKE_BUILD_TYPE=Release -DBUILD_EDGE=ON -G %2 .. & cmake --build . --config Release --target %3
 ) else (
-  "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\vcvarsall.bat" %1 & cmake -DCMAKE_EXPORT_COMPILE_COMMANDS=ON -DCMAKE_BUILD_TYPE=Release -G "NMake Makefiles" .. & jom %2
+  "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\vcvarsall.bat" %1 & cmake -DCMAKE_EXPORT_COMPILE_COMMANDS=ON -DCMAKE_BUILD_TYPE=Release -G %2 .. & cmake --build . --config Release --target %3
 )
